a784b72011d48dd671df167572e45eeddb597e49
rkozelj <romi.kozelj@gmail.com>
2018-06-28 12:02:50 +0200

    result file for function 'napisi_na_dat'

---
+++ src/podnaloga.py
@@ -0,0 +1,16 @@
+# =====================================================================@015027=
+# Na spletni strani `https://www.ncbi.nlm.nih.gov/guide/howto/dwn-genome/`
+# poišči genski zapis z oznako KT232076.1 in v obliki niza povej za
+# katero vrsto bakterije gre.
+# 
+#     # Resitev bo oblike:
+#     "Enterobacteria *** lambda"
+#     # kjer tri zvezdice zamenjaj za ustrezno ime.
+# -----------------------------------------------------------------------------
+# # Resitev bo oblike:
+# "Enterobacteria *** lambda"
+# # kjer tri zvezdice zamenjaj za ustrezno ime.
+# =============================================================================
+"Enterobacteria phage lambda"
+
+Check.part()

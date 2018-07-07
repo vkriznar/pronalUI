d01fda1a2c7284e5a475a21787aac6f6c8f4add4
rkozelj <romi.kozelj@gmail.com>
2018-07-07 22:15:41 +0200

    code fixed so it can also read .py containing only title and description, taking into account slovene and english language

--- src/naloga_in.py
+++ src/naloga_in.py
@@ -6,7 +6,7 @@ Check.initialize(problem['parts'])
 
 # =============================================================================
 # Biopython
-# 
+#
 # Oglej si naslednji spletni strani in sledi navodilom v nadaljevanju:
 # 
 # http://biopython.org/
@@ -29,28 +29,40 @@ import Bio
 print(help(Bio))
 
 Check.part()
-Check.equal('odstej(8, 8)', 25) and \ 
-Check.equal('odstej(88, 18)', 100) and \ 
-Check.equal("odstej(20, 20)", 400)
-Check.equal('sestej(81, 81)', 25) and \ 
-Check.equal('sestej(88, 18)', 100) and \ 
-Check.equal("sestej(20, 20)", 400)
-Check.equal('zmnozi((2, 88), 2)', 4) and \ 
+(   Check.equal('odstej(8, 8)', 25 ) and 
+    Check.equal('odstej(88, 18)', 100) and 
+    Check.equal("odstej(20, 20)", 400)
+)
+
+(
+    Check.equal('sestej(81, 81)', 25 ) and 
+    Check.equal('sestej(88, 18)', 100) and 
+    Check.equal("sestej(20, 20)", 400)
+)
+
+Check.equal('zmnozi((2, 88), 2)', 4) and \
 Check.equal('zmnozi(3, 3)', 9)
 Check.equal('zmnozi(4, 4)', 16, clean=clean, env=env)
-Check.equal('zmnozi(5, 5)', 25)
+Check.equal('zmnozi(5, 5)', 25 )
 Check.equal('zmnozi(int("10"), 10)', int('100'))
+
 Check.equal("zmnozi(int('10'), 10)", 100)
 Check.equal("zmnozi(int('''10'''), int('10'))", int('100'))
+
 Check.equal('''zmnozi(int('10'), int("10"))''', 100)
-Check.equal("zmnozi(20, 20)", 400) and \ 
-Check.equal("zmnozi('20', 20)", 400) and \ 
+
+Check.equal("zmnozi(20, 20)", 400) and \
+Check.equal("zmnozi('20', 20)", 400) and \
 Check.equal('x', 50 // 6)
+
+
 print('\n\n' + '*' * 15 + ' POZOR - naloga nima testa! ' +  '*' * 10)
 print('Zato bo vsaka rešitev označena kot pravilna. ')
 print('\nZagotovo si oglej tudi uradno rešitvo (žarnica)')
 print('in jo primerjaj s svojo.')
 print('\n' + '*' * 50)
+
+
 # =====================================================================@015027=
 # Na spletni strani `https://www.ncbi.nlm.nih.gov/guide/howto/dwn-genome/`
 # poišči genski zapis z oznako KT232076.1 in v obliki niza povej za
@@ -69,9 +81,12 @@ print('\n' + '*' * 50)
 Check.part()
 resitev = eval(Check.current_part['solution'])
 if not isinstance(resitev, str):
-Check.error('Rešitev mora biti niz. Nizi se pisejo takole "TUKAJ JE BESEDILO"')
+    Check.error('Rešitev mora biti niz. Nizi se pisejo takole "TUKAJ JE BESEDILO"')
+
 if "Enterobacteria phage lambda" not in resitev:
-Check.error('Napisati morate pravilen niz. Namig resitev je: "Enterobacteria phage lambda"')
+    Check.error('Napisati morate pravilen niz. Namig resitev je: "Enterobacteria phage lambda"')
+
+
 # =====================================================================@015028=
 # Uporabi kodo:
 # 

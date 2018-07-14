a158dd6fdb538999f0d04a9214748832e391a134
vkriznar <krizovid@gmail.com>
2018-07-14 14:19:45 +0200

    dodal other test

--- pronalUI-bottle/main.py
+++ pronalUI-bottle/main.py
@@ -3,6 +3,7 @@ import webbrowser
 
 static_directory = "./static"
 sklop = []
+active_test = "chkeql"
 
 
 
@@ -32,7 +33,9 @@ def index_post():
 
 @get("/index/podnaloga/")
 def podnaloga():
-    return template("podnaloga.html", napaka=None, description="", code="", precode="", rows=None, testi=False)
+    global active_test
+    active_test = "chkeql"
+    return template("podnaloga.html", napaka=None, description="", code="", precode="", rows=None, testi=False, active_test=active_test)
 
 @post("/index/podnaloga/")
 def podnaloga_post():
@@ -40,7 +43,7 @@ def podnaloga_post():
     koda = request.forms.koda
     prekoda = request.forms.prekoda
     if all([opis, koda, prekoda]):
-        sklop.append([opis, prekoda, koda, [[], []]])
+        sklop.append([opis, prekoda, koda, [[], [], []]])
         redirect("/index/podnaloga/testi/")
     else:
         return template("podnaloga.html", napaka="Vsa polja morajo biti izpolnjena")
@@ -48,16 +51,23 @@ def podnaloga_post():
 
 @get("/index/podnaloga/testi/")
 def podnaloga_testi():
+    print(sklop[-1][3][2])
     return template("podnaloga.html", description=sklop[-1][0], precode=sklop[-1][1],
-                    code=sklop[-1][2], napaka=None, rows=sklop[-1][3], testi=True)
+                    code=sklop[-1][2], napaka=None, rows=sklop[-1][3], testi=True, active_test=active_test)
 
 @post("/index/podnaloga/testi/")
 def podnaloga_testi_post():
     "Če test nima določenega atributa vrne None namest praznega niza"
+    global active_test
     if request.forms.tipTesta == "chkeql":
         sklop[-1][3][0].append([request.forms.stevilka, request.forms.niz, request.forms.rezultat])
+        active_test = "chkeql"
     elif request.forms.tipTesta == "chksct":
         sklop[-1][3][1].append([request.forms.stevilka, request.forms.niz1, request.forms.niz2])
+        active_test = "chksct"
+    else:
+        sklop[-1][3][2].append(zamenjaj(request.forms.other))
+        active_test = "other"
     redirect("/index/podnaloga/testi/")
 
 @get("/pretvori/")
@@ -66,6 +76,17 @@ def pretvori():
     for array in sklop:
         print(array)
     return HTTPResponse("Uspelo ti je!")
+
+def zamenjaj(string):
+    chars = list(string)
+    for i in range(0, len(chars)):
+        if chars[i] == "\n":
+            chars[i] = "<br>"
+        if chars[i] == " ":
+            chars[i] = "&nbsp"
+        if chars[i] == "\r":
+            chars[i] = ""
+    return "".join(chars)
 
 
 run(host='localhost', port=8080, debug=True)

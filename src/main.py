30d1e73a67b6c2771b9cf8adda777e085a8dbde1
vkriznar <krizovid@gmail.com>
2018-08-13 10:55:44 +0200

    kometiranje kode, olepsave kode, brisanje nepomembne kode

--- src/main.py
+++ src/main.py
@@ -3,13 +3,13 @@ import webbrowser
 from Problem import Problem
 from ProblemPart import ProblemPart, CheckEqual, CheckSecret
 import time
-import re
 import json
 import bottle
 
 static_directory = "./static"
+
+"Global spremenljivke"
 active_test = "chkeql"
-testi = False
 je_bila_izbrisana = False
 podnaloge_za_osvezit = []
 preostevilcenje = []
@@ -19,103 +19,75 @@ popup = False
 app = bottle.default_app()
 BaseTemplate.defaults['get_url'] = app.get_url
 
-# webbrowser.open('http://localhost:8080/index/')
 webbrowser.open('http://localhost:8080/')
 
+"Funckija ki redirecta iz '/' na zacetno stran"
 @route('/')
 def index():
     redirect('/index/')
 
+
+"Staticni route za css in js file"
 @route('/static/<filename:path>', name='static')
 def serve_static(filename):
     return static_file(filename, root=static_directory)
 
+
+"Zacetna stran kjer uporabnik izbire datoteko za urejanje"
 @get("/index/")
 def index():
     global popup
     return template("index.html", popup=popup)
 
+
+"Route kjer se iz izbrane datoteke parsa in se preveri ce je"
 @post('/upload')
 def upload():
     global file
     global popup
-    global file_name # to zaenkrat rabimo, za pisat na datoteko
+    global problem
     file = request.files.get('file')
-    file_name = file.filename # to zaenkrat rabimo, za pisat na datoteko
+    "preverimo ce je vnesena datoteka res python datoteka"
     if os.path.splitext(file.filename)[1] != ".py":
+        """ce ni, ga redirectamo nazaj na osnovno stran, ter na spletni strani se mu pojavi popup ki mu pove, da so
+         dovoljena samo python datoteke"""
         popup = True
         redirect("/index/")
     else:
         popup = False
-        return redirect("/index/nova_naloga")
+        problem = Problem.read_filefile(file.file)
+        "za vsako podnalogo se odpre zavihek z to podnalogo, time sleep je zato da se v pravilnem vrstnem redu odprejo"
+        for i in range(len(problem.parts)):
+            time.sleep(0.05)
+            webbrowser.open('http://localhost:8080/index/naloga/podnaloga{}/'.format(i + 1))
 
+        redirect("/index/naloga/")
+
+
+"Funkcija ki preostevilci vrstni red podnalog"
 @get('/preostevilci<part_num><num>')
 def preostevilci(part_num, num):
     global preostevilcenje
+
     part_num = int(part_num)
     preostevilcenje.append({part_num, int(num)})
-    print(preostevilcenje)
-    """global podnaloge_za_osvezit
-    part_num = int(part_num)
-    num = int(num)
-    prvic = True
-    print(podnaloge_za_osvezit)
-    if part_num > num:
-        if part_num in podnaloge_za_osvezit:
-            podnaloge_za_osvezit.remove(part_num)
-        for i in range(num+1, part_num):
-            if i in podnaloge_za_osvezit:
-                podnaloge_za_osvezit.remove(i)
-            else:
-                podnaloge_za_osvezit.append(-i)
-    else:
-        if num in podnaloge_za_osvezit:
-            return HTTPResponse("Uspelo ti ni!")
-        if num in podnaloge_za_osvezit:
-            podnaloge_za_osvezit.remove(num)
-            podnaloge_za_osvezit.append(str(num))
-        for j in range(part_num+1, num+1):
-            if j in podnaloge_za_osvezit:
-                if prvic:
-                    podnaloge_za_osvezit.remove(j-1)
-                    prvic = False
-                podnaloge_za_osvezit.remove(j)
-                podnaloge_za_osvezit.append(str(j))
-            else:
-                if not prvic:
-                    podnaloge_za_osvezit.append(str(j))
-                else:
-                    podnaloge_za_osvezit.append(j)
-    print(podnaloge_za_osvezit)"""
+
     redirect("/index/naloga/podnaloga{}/".format(part_num))
 
 
-
-## tole spodi ni ok !!
-"Zacetna stran kjer uporabnik izbire, ali bo ustvaril novo datoteko ali bo vnesel ze obstojeco datoteko za urejanje"
-@get("/index/nova_naloga")
-def nova_naloga():
-    global problem
-    global file
-
-    problem = Problem.read_filefile(file.file)
-
-    for i in range(len(problem.parts)):
-        time.sleep(0.05)
-        webbrowser.open('http://localhost:8080/index/naloga/podnaloga{}/'.format(i + 1))
-    redirect("/index/naloga/")
-
+"stran, kjer uporabnik vpise naslov naloge, in opis naloge"
 @get("/index/naloga/")
 def naloga():
     global problem
-    adress = problem.title
-    description = problem.description
     
-    return template("naloga.html", napaka=None, adress=adress, description=description, code="")
+    return template("naloga.html", napaka=None, adress=problem.title, description=problem.description, code="")
 
+
+"Route, kjer se shrani kar je uporabnik vpisal za naslov in opis podnaloge. Naloga mora imeti naslov."
 @post("/index/naloga/")
 def naloga_post():
     if request.forms.naslov:
+        "shrani si vpisan naslov in opis naloge"
         problem.title = request.forms.naslov
         problem.description = request.forms.opis
         return template("naloga.html", napaka=None, adress=problem.title, description=problem.description)
@@ -123,89 +95,67 @@ def naloga_post():
         return HTTPResponse("Uspelo ti ni!")
 
 
+"Funckija, ki izbrise izbrano podnalogo"
 @get("/index/naloga/podnaloga_izbrisi<part_num>/")
 def podnaloga_izbrisi(part_num):
     global problem
     global je_bila_izbrisana
     global podnaloge_za_osvezit
+    "part_num dobimo v obliki stringa zato ga je najprej treba pretvoriti v intiger"
     part_num = int(part_num)
     del problem.parts[part_num-1]
+    """ko smo zbrisali eno izmed podnalog moramo vse podnaloge z vecjim part_num, njihov part_num zmanjsati za ena
+    to naredimo tako da shranimo vecje podnaloge v list in potem ko se stran osvezi preverimo, ce je ta stran v tem
+    listu in ce je ji zmanjsamo part_num za ena"""
     podnaloge_za_osvezit = [i for i in range(part_num+1, len(problem.parts)+2)]
 
     return HTTPResponse("Uspelo ti je izbrisati nalogo {0}!".format(part_num))
 
 
+"Stran z podnalogo, 'part_num' oznacuje specificno podnalogo na kateri se trenutno nahajamo"
 @get("/index/naloga/podnaloga<part_num>/")
 def podnaloga(part_num):
     global problem
-    part_num = int(part_num)
     global active_test
     global trenutno_osvezena
     global podnaloge_za_osvezit
+
+    part_num = int(part_num)
     active_test = "chkeql"
 
-        
     if part_num > len(problem.parts):
-        # problem_part = problem.parts[part_num - 2] # napaka
         problem_part = problem.parts[-1]
-        # part_num = 1
     else:
         problem_part = problem.parts[part_num-1]
-    
-    description = problem_part.description
-    solution = problem_part.solution
-    precode = problem_part.precode
-    tests = problem_part.tests
-    # print(part_num, tests)
 
     if podnaloge_za_osvezit:
         if part_num in podnaloge_za_osvezit:
             if trenutno_osvezena != part_num:
                 podnaloge_za_osvezit.remove(part_num)
+                """da se nebi part_num 3 osvezil na 1(npr. ce izbrisemo podnalogo 1), moramo prepriciti da se prvic 
+                osvezi podnaloga z part_num 2 zato to shranimo v trenutno_osvezena"""
                 trenutno_osvezena = part_num-1
                 redirect("/index/naloga/podnaloga{}/".format(part_num-1))
             else:
                 trenutno_osvezena = None
 
-        """if -part_num in podnaloge_za_osvezit:
-            if trenutno_osvezena != part_num:
-                podnaloge_za_osvezit.remove(-part_num)
-                trenutno_osvezena = part_num+1
-                redirect("/index/naloga/podnaloga{}/".format(part_num+1))
-            else:
-                trenutno_osvezena = None
-        if str(part_num) in podnaloge_za_osvezit:
-            if trenutno_osvezena != part_num:
-                podnaloge_za_osvezit.remove(str(part_num))
-                trenutno_osvezena = part_num-2
-                redirect("/index/naloga/podnaloga{}/".format(part_num-2))
-            else:
-                trenutno_osvezena = None"""
-
-
-
     return template("podnaloga.html", napaka=None,
-                    description=description, code=solution,
-                    precode=precode, tests=tests, testi=testi,
-                    active_test=active_test, changes=None, part_num=part_num)
+                    description=problem_part.description, code=problem_part.solution,
+                    precode=problem_part.precode, tests=problem_part.tests,
+                    active_test=active_test, part_num=part_num)
 
 
-
-
+"Funkcija ki procesira vsa urejanja na tocno dolocenem testu"
 @get("/index/naloga/podnaloga<part_num>/<test_type>-<edit>-<group_id>-<i>/")
 def edit_tests(part_num, test_type, edit, group_id, i):
+    global problem
+
     part_num = int(part_num)
     group_id = int(group_id)
     i = int(i)
 
-    global problem
-    global testi
-    print(edit,group_id, i)
     problem_part = problem.parts[part_num-1]
     tests = problem_part.tests
-    #print(tests["check_equal"])
-    #print("PREJ:", tests["check_equal"][group_id])
-    #print(tests["check_equal"][group_id][i])
     
     if test_type == "chkeql":
         test_group = tests["check_equal"][group_id]
@@ -214,64 +164,57 @@ def edit_tests(part_num, test_type, edit, group_id, i):
         test_group = tests["check_secret"][group_id]
         test = tests["check_secret"][group_id][i]
 
-   
     if edit == "delete":
         problem_part.remove_test(test)
     elif edit == "move":
         problem_part.description += "\n\n    >>> " + test.expression + "\n    " + test.output
 
-    elif edit=="move_down":
-        test_type = "check_equal" if test_type=="chkeql" else "check_secret"
-        if i==len(test_group)-1:   
+    elif edit == "move_down":
+        test_type = "check_equal" if test_type == "chkeql" else "check_secret"
+        if i == len(test_group)-1:
             problem_part.move_test_group_down(test_type, group_id)
         else: 
             problem_part.move_test_within_group_down(test_type, group_id, i)
             
-    elif edit=="move_up":
-        test_type = "check_equal" if test_type=="chkeql" else "check_secret"
-        if i==0: 
+    elif edit == "move_up":
+        test_type = "check_equal" if test_type == "chkeql" else "check_secret"
+        if i == 0:
             problem_part.move_test_group_up(test_type, group_id)
         else: 
             problem_part.move_test_within_group_up(test_type, group_id, i)
-        
-    #print("POTEM:", tests["check_equal"][group_id])
-        
-    
-    testi = True
+
     redirect("/index/naloga/podnaloga{}/".format(part_num))
 
 
+"Route, ki je v pomoc zato, da ko se ustvari nova podnaloga ji funkcija dodeli pravi part_num"
 @get("/index/naloga/podnaloga/")
 def podnaloga_get_def():
     global problem
+
     part_num = len(problem.parts)
     problem.new_problem_part()
-    redirect("/index/naloga/podnaloga{}/".format(part_num +1 ))
+    redirect("/index/naloga/podnaloga{}/".format(part_num + 1))
 
+
+"Fukcija, ki shrani vse vnesene spremembe za doloceno podnalogo"
 @post("/index/naloga/podnaloga<part_num>/")
 def podnaloga_post(part_num):
     global problem
-    global testi
     global preostevilcenje
-    changes = []
-    testi = False
+
     part_num = int(part_num)
     problem_part = problem.parts[part_num-1]
     tests = problem_part.tests
 
     if request.forms.opis:
-        #print("OPIS vmesnik:", request.forms.opis)
         problem_part.description = request.forms.opis
-        #print("OPIS description:", problem_part.description)
         problem_part.solution = request.forms.koda
         problem_part.precode = request.forms.prekoda
-    if request.forms.prekoda_gor == "True":     #To ni isto kot boolean==True, ker je True string
+    if request.forms.prekoda_gor == "True":     # To ni isto kot "boolean == True", ker je "True" string
         problem_part.precode_to_description()
-
 
     else:
         global active_test
-        testi = True
         if request.forms.tipTesta == "chkeql":
             # in python format (0 starts)
             test_num = int(request.forms.stevilka) - 1
@@ -288,9 +231,6 @@ def podnaloga_post(part_num):
             # in python format (0 starts)
             test_num = int(request.forms.stevilka) - 1
             check_secret_test = CheckSecret(request.forms.niz1, request.forms.niz2)
-
-            
-            
             if test_num == len(tests["check_secret"]):
                 tests["check_secret"].append([check_secret_test])
             elif test_num > len(tests["check_secret"]):
@@ -300,36 +240,31 @@ def podnaloga_post(part_num):
 
             active_test = "chksct"
         else:
-            tests["other"] += (request.forms.other)
-
+            tests["other"] += request.forms.other
 
     if request.forms.changes:
         json_dict = json.loads(request.forms.changes)
-        print(json_dict)
 
         for test_type in json_dict:
             for test_data in json_dict[test_type]:
-                #print("spremembe: ", test_data)
                 group_id = int(test_data["group_id"]) - 1
                 i = int(test_data["index"]) - 1
                 expression = test_data["expression"]
                 output = test_data["output"] if test_type == "check_equal" else test_data["other"]
                 problem_part.change_test_by_id(test_type, group_id, i, expression, output)
-                #print("spremembe2: ", problem_part.tests[test_type][group_id][i])
+
+    redirect("/index/naloga/podnaloga{}/".format(part_num))
 
 
-    redirect("/index/naloga/podnaloga{}/".format(part_num)) # treba je returnat template, ki bo vrnu dano podnalogo s spremenjenimi testi
-
-
-
+"Route, ki odda nalogo in jo shrani na racunalnik"
 @get("/pretvori/")
 def pretvori():
-    global file_name
-    file_name = "../edit_files/"+file_name
+    global file
+    "shranimo nalogo na racunalnik. To je treba se spremeniti, zdaj je tocno doloceno mesto!"
+    file_name = "../edit_files/" + file.filename
     problem.write_on_file(file_name[:-6] + "_out.py")
-    #print(problem)
-    return HTTPResponse("Uspelo ti je!")
 
+    return HTTPResponse("Uspelo ti je!")
 
 
 run(host='localhost', port=8080, debug=True)
